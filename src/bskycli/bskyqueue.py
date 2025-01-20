af2470c2c59e2f0ab3070d5f4bc2ba9b4732a747
Michael Lamertz <michael.lamertz@gmail.com>
2025-01-20 21:20:16 +0100

    Import cleanups, make seconds optional

--- src/bskycli/bskyqueue.py
+++ src/bskycli/bskyqueue.py
@@ -1,21 +1,16 @@
 import argparse
-import os.path
-import re
 import sys
 import time
 
 from pathlib import Path
-from shutil import copyfile, move
 
-import bskycli.config as C
-from bskycli.lock import lock
 from bskycli.post import post
 
 def main():
     now = time.strftime('%F-%T')
 
     cmdline = argparse.ArgumentParser(description='Schedule a bluesky posts')
-    cmdline.add_argument('-t', '--at', type=str, default=now, help='Earliest time to post.  Format is YYYY-MM-DD-HH:MM:SS.  Default is now.')
+    cmdline.add_argument('-t', '--at', type=str, default=now, help='Earliest time to post.  Format is YYYY-MM-DD-HH:MM[:SS].  Default is now.')
     cmdline.add_argument('textfile', type=Path, help='File containing the post text.  Use "-" for stdin.')
     cmdline.add_argument('images', nargs='*', type=Path, help='Optional list of up to 4 images')
 
