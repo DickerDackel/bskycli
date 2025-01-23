e080cc3544bb6309b053842fe9c7addda58e707f
Michael Lamertz <michael.lamertz@gmail.com>
2025-01-23 10:22:21 +0100

    Fixed session saving/loading, better ratelimit error

--- src/bskycli/auth.py
+++ src/bskycli/auth.py
@@ -1,6 +1,9 @@
 import os.path
 
+from time import localtime
+
 from atproto import SessionEvent
+from atproto.exceptions import RequestException
 
 import bskycli.config as C
 
@@ -8,9 +11,9 @@ from bskycli.config import config_dir
 
 
 def get_credentials():
-    credentials_file = os.path.join(config_dir(), 'credentials')
+    credentials_file = config_dir() / 'credentials'
 
-    with open(credentials_file) as f:
+    with credentials_file.open() as f:
         user, password = f.readline().strip().split(':')
         return user, password
 
@@ -27,25 +30,35 @@ def get_session():
 def save_session(session):
     session_file = C.data_dir() / 'session.txt'
     with session_file.open('w') as f:
-        os.chmod(session_file.fileno(), 0o600)
+        os.chmod(f.fileno(), 0o600)
         f.write(session)
 
 
 def session_change(event, session):
     print(f'Session changed: {event=} - {session=}')
     if event in (SessionEvent.CREATE, SessionEvent.REFRESH):
-        save_session(session)
+        save_session(session.export())
 
 
 def login(client):
     client.on_session_change(session_change)
 
-    if session := get_session():
-        print('Reusing existing session')
-        client.login(session_string=session)
-    else:
-        print('New login')
-        client.login(*get_credentials())
+    try:
+        if session := get_session():
+            print('Reusing existing session')
+            client.login(session_string=session)
+        else:
+            print('New login')
+            client.login(*get_credentials())
+    except RequestException as e:
+        if e.content.error  == 'RateLimitExceeded':
+            reset_time = localtime(e.headers['ratelimit-reset'])
+            policy = e.headers['ratelimit-policy']
+            raise SystemExit(f'{e.content.message} - Retry {reset_time}  Polocy: {policy}')
+        else:
+            print('Haven\'t had this exception yet...')
+            raise e
+
 
 __all__ = [
     get_credentials,
