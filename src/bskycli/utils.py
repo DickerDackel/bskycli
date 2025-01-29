1ec2e55f2053174966feaddeb239b29412c89efa
Michael Lamertz <michael.lamertz@gmail.com>
2025-01-29 14:34:45 +0100

    Fixed rx handling
    bsky docs suggest matching on bytes instead of strings, because the
    index positions of facets are byte based.  But this kills match patterns
    like \w which won't match umlauts anymore in a bytes sequence.
    
    The solution is to match in utf8, then convert the prefix before the
    match to bytes and use that length as start.  Same conversion with
    finding the end.

--- src/bskycli/utils.py
+++ src/bskycli/utils.py
@@ -1,16 +1,32 @@
 import re
 
+from typing import NamedTuple
+
 from atproto import IdResolver, models
 
 RX_TAG = re.compile(r'#\w+')
 RX_MENTION = re.compile(r'@([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?')
 RX_LINK = re.compile(r'https?://[\w\-_#%&;./]+')
 
+
+class Facet(NamedTuple):
+    start: int
+    end: int
+    match: str
+
+
 def find_facets(rx, text):
     res = []
-    text_bytes = text.encode('UTF-8')
-    for item in rx.finditer(text_bytes):
-        res.append((item.start(), item.end(), item.group().decode('UTF-8')))
+
+    # Note: The bsky/atproto docs convert the utf8 string to bytes and match
+    # on that, but that kills e.g. matching \w patterns.  So instead I use
+    # normal string matching here, then encode the prefix to bytes and use
+    # its length as start.  Same with end.  This way, I have the full utf8 rx
+    # matching with the proper byte positions required by bsky.
+    for item in rx.finditer(text):
+        bytes_start = len(text[:item.start()].encode('utf8'))
+        bytes_end = bytes_start + len(item.group().encode('utf8'))
+        res.append(Facet(bytes_start, bytes_end, item.group()))
 
     return res
 
@@ -19,31 +35,30 @@ def pull_out_facets(text):
     for facet in find_facets(RX_TAG, text):
         facets.append(
             models.AppBskyRichtextFacet.Main(
-                features=[models.AppBskyRichtextFacet.Tag(tag=facet[2][1:])],
-                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=facet[0], byte_end=facet[1]),
+                features=[models.AppBskyRichtextFacet.Tag(tag=facet.match[1:])],
+                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=facet.start, byte_end=facet.end),
             )
         )
 
     resolver = IdResolver()
     for facet in find_facets(RX_MENTION, text):
-        did = resolver.handle.resolve(facet[2][1:])
+        did = resolver.handle.resolve(facet.match[1:])
         if did:
             facets.append(
                 models.AppBskyRichtextFacet.Main(
                     features=[models.AppBskyRichtextFacet.Mention(did=did)],
-                    index=models.AppBskyRichtextFacet.ByteSlice(byte_start=facet[0], byte_end=facet[1]),
+                    index=models.AppBskyRichtextFacet.ByteSlice(byte_start=facet.start, byte_end=facet.end),
                 )
             )
         else:
-            print(f'handle {facet[2]} not found')
+            print(f'handle {facet.match} not found')
 
     for facet in find_facets(RX_LINK, text):
         facets.append(
             models.AppBskyRichtextFacet.Main(
-                features=[models.AppBskyRichtextFacet.Link(uri=facet[2])],
-                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=facet[0], byte_end=facet[1]),
+                features=[models.AppBskyRichtextFacet.Link(uri=facet.match)],
+                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=facet.start, byte_end=facet.end),
             )
         )
 
     return facets
-
